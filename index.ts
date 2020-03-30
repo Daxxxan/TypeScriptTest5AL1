0def6224938d670358f76a215fde02a44a69cdcf
Unknown <alex-bad97@hotmail.fr>
2020-03-30 11:50:47 +0200

    Add jest + first tests

--- index.ts
+++
@@ -1,4 +0,0 @@
-const Pokemon = require("./Pokemon");
-
-let pokemon = new Pokemon("pikapika");
-console.log(pokemon.name);
---
+++ src/main/index.ts
@@ -0,0 +1,6 @@
+import { Pokemon } from "./Pokemon";
+
+let pikachu = new Pokemon("Pikachu");
+let bulbizarre = new Pokemon("Bulbizarre");
+console.log(pikachu.name);
+console.log(bulbizarre.name);
